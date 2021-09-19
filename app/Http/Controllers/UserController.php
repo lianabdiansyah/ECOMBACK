915229b93a16fe2fa805980c7fbb1c0a2538f189
liancmd <lianabdiansyahtanjung@gmail.com>
2021-09-19 23:39:20 +0700

    menambahkan testing api login backend

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    function register(request $request) {
+        $user = new User;
+        $user->nama = $request->input("nama");
+        $user->email = $request->input("email");
+        $user->password = Hash::make($request->input("password"));
+        $user->save();
+        return $user;
+        // return $request->input();
+    }
+
+    function login(){
+        return "login berhasil";
+    }
+}
